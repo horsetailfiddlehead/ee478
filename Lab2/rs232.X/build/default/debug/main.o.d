fd3d7528cbbc387e94f2f472155c4e150c00891e
rynmcd <rblckx@gmail.com>
2014-04-23 19:35:17 -0700

    UART
    Stuff

--- Lab2/rs232.X/build/default/debug/main.o.d
+++ Lab2/rs232.X/build/default/debug/main.o.d
@@ -1,3 +1,3 @@
-build/default/production/main.o:  \
+build/default/debug/main.o:  \
 	C:/Users/rynmcd/Documents/GitHub/ee478/Lab2/rs232.X/main.c  \
 
--- Lab2/rs232.X/main.c
+++ Lab2/rs232.X/main.c
@@ -6,6 +6,8 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <portb.h>
 #include <p18f25k22.h>
 //#include <sw_uart.h>
 #include <usart.h>
@@ -35,25 +37,69 @@ void DelayRXHalfBitUART();
 void DelayRXBitUART();
 */
 
+void readBytesUntil(char* myStorage, char stopChar, int size);
+
+
+
 void main(void)
 {
-    unsigned char myWord[5];
-    myWord[0] = 'a';
-    myWord[1] = 's';
-    myWord[2] = 's';
-    myWord[3] = ' ';
-    myWord[4] = '\0';
 
+    unsigned char myWord[] = "moo ";
+    unsigned char myInputWord[64];
+    char message;
+    int i = 0;
+    
     //  Set all of PORTC as outputs. TX and RX must be set as outputs first
-    TRISC=0x00;
+    TRISC=0x80;
+    ANSELC=0x00;
 
     //  Configure UART
-    Open1USART(USART_TX_INT_OFF & USART_RX_INT_OFF & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_BRGH_HIGH, 129);
+
+    Open1USART(USART_TX_INT_OFF & USART_RX_INT_ON & USART_ASYNCH_MODE & USART_EIGHT_BIT & USART_BRGH_HIGH, 129);
+
+    while(1) {
+        readBytesUntil(myInputWord, '\r', 64);
+        putc1USART('\r');
+        putc1USART('\n');
+        puts1USART(myInputWord);
+    }
+
+}
+
+// Precondition: USART 1 is open & configured
+void readBytesUntil(char* myStorage, char stopChar, int size) {
+    int i = 0;
+    char message;
+
+    while(!DataRdy1USART());
+    message = getc1USART();
+    putc1USART(message);
+    while(message != stopChar && i < (size - 1)) {
+        myStorage[i] = message;
+        i++;
+        while(!DataRdy1USART());
+        message = getc1USART();
+        putc1USART(message);
+    }
+
+    myStorage[i] = '\0';
+    i = 0;
+}
+/*
+    while(1) {
+        while(!DataRdy1USART());
+        message=getc1USART();
+        putc1USART(message);
+        Delay1KTCYx(4);
+    }
+
+
+    /* Write Cow
     while(1)
     {
         puts1USART(myWord);
     }
-}
+    */
 
 /*
 void DelayTXBitUART() {
