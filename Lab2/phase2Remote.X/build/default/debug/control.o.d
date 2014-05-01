52ec43361e5f96e9df85b8df99db7b0af25123d9
castia <castillo.alyanna@gmail.com>
2014-04-30 20:41:51 -0700

    remote control works!
    next: error state checking and update user input

---
+++ Lab2/phase2Remote.X/build/default/debug/control.o.d
@@ -0,0 +1,7 @@
+build/default/debug/control.o:  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/control.c  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/SRAM.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/globals.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/control.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/interrupts.h  \
+
--- Lab2/phase2Remote.X/main.c
+++ Lab2/phase2Remote.X/main.c
@@ -29,9 +29,9 @@
 #define DEF_PWM 20
 /***********************Global Setup*****************/
 // System variables
-unsigned int setSpeed = 0; // the user defined motor speed
+unsigned int setSpeed = 0; // the user defined motor speed (from local node)
 unsigned int actualSpeed = 0; // Motor speed given from the remote (was motorSpeed)
-unsigned int controllerSpeed = DEF_PWM; // Motor speed send to the remote node by the local
+unsigned int controllerSpeed = DEF_PWM; // PWM motor speed
 unsigned int errorState = 4; // The current motor state is off
 
 // User Input
@@ -71,7 +71,7 @@ void i2cISR(void) {
         temp = SSP2BUF;
     }
     if (SSP2STATbits.D_A == 1 && SSP2STATbits.BF == 1) {
-        *ourGlobal.setSpeed = SSP2BUF;
+        *ourGlobal.setSpeed = SSP2BUF; // new motor set speed
     }
 
     PIR3bits.SSP2IF = 0;
@@ -104,13 +104,16 @@ void setupPWM() {
 void main() {
     unsigned int temp = 0;
     unsigned int correct = 0;
+    unsigned int delay = 0;
+
+    // setup i2c
     setupIncoming();
     setupOutgoing();
+
+    //setup PWM
     setupPWM();
 
-
-
-    //    // adc
+    // Setup ADC
     TRISCbits.RC2 = 1;
     TRISCbits.RC5 = 0;
     ANSELCbits.ANSC5 = 0;
@@ -132,39 +135,51 @@ void main() {
             *ourGlobal.processFlag = 1;
             *ourGlobal.SRAMflag = 0;
         }
-        // Process user's "set speed" command
-        if (*ourGlobal.processFlag == 1) {
-            SetDCPWM4(5 * (readData(1)));
-            *ourGlobal.processFlag = 0;
-        }
-        // Transfer actual speed to local node
-        if (*ourGlobal.i2cFlag == 1) {
-            // Work on math here. We can't go above 65535 in th emath or it breaks
-            // 16 bit max int. So 100 * 65 is the max doing it a conventional way
-            if (*ourGlobal.actualSpeed >= 50) {
-                correct = 50 * (*ourGlobal.actualSpeed) >> 8; //correct = 2 * (2*((50 * *ourGlobal.actualSpeed) / 1023));
-            } else {
-                correct = 50 * (*ourGlobal.actualSpeed) >> 8; //2 * ((100 * *ourGlobal.actualSpeed) / 1023);
-            }
-            Delay1KTCYx(1);
-            runLocalI2C(&correct);
-            *ourGlobal.i2cFlag = 0;
-            // TODO: Calculate and send error
-        }
 
         // Measure Motor
         Delay10KTCYx(8);
-        PORTCbits.RC5 = 1;
+        PORTCbits.RC5 = 1; // conversion indicator
         ConvertADC();
         while (BusyADC());
         temp = ReadADC();
-        PORTCbits.RC5 = 0;
-        if ((temp - 5 > *ourGlobal.actualSpeed) || (*ourGlobal.actualSpeed > temp + 5)) {
-            *ourGlobal.actualSpeed = temp;
+        temp = (50 * (temp)) >> 8;
+        PORTCbits.RC5 = 0; // conversion indicator
+
+        // only makes sense to update ADC if significantly different than
+        // prior reading
+        if ((temp - 2 > *ourGlobal.actualSpeed) || (*ourGlobal.actualSpeed > temp + 2)) {
+            correct = temp;
+            *ourGlobal.actualSpeed = correct;
             *ourGlobal.i2cFlag = 1;
         }
+
+        if (*ourGlobal.actualSpeed < (*ourGlobal.setSpeed - 2) ||
+                (*ourGlobal.setSpeed + 2) < *ourGlobal.actualSpeed) {
+            controller(&ourGlobal); // compute the control function and adjust pwm
+        }
+
+
+        // Process updated controller speed
+        if (*ourGlobal.processFlag == 1) {
+            *ourGlobal.processFlag = 0;
+            writeData(2, *ourGlobal.controllerSpeed); // update the motor output
+        }
+        SetDCPWM4(5 * readData(2));
+
+        // Transfer actual speed to local node
+        if (*ourGlobal.i2cFlag == 1) {
+            *ourGlobal.i2cFlag = 0;
+            // Adjust actual speed for tranmittal as 8 bits. We can't go above
+            // 65535 in th emath or it breaks 16 bit max int.
+            // So 100 * 65 is the max doing it a conventional way
+            //correct = 50 * ((*ourGlobal.actualSpeed) >> 8); //correct = 2 * (2*((50 * *ourGlobal.actualSpeed) / 1023));
+
+            Delay1KTCYx(1);
+            runLocalI2C(ourGlobal.actualSpeed);
+            // TODO: Calculate and send error
+        }
+
         Delay1KTCYx(1);
-        //receiveData();
     }
 }
 
