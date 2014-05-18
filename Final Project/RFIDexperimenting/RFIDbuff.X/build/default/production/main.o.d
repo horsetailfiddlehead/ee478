d6ec55100a7fafc6f076a6346d90b4dac189b0b2
castia <castillo.alyanna@gmail.com>
2014-05-18 13:35:55 -0700

    Updated buffer project
    Start hyperterm at 115200 and type a command followed by CTRL+Enter.
    Then, press ENTER to send it.

--- Final Project/RFIDexperimenting/RFIDbuff.X/build/default/production/main.o.d
+++ Final Project/RFIDexperimenting/RFIDbuff.X/build/default/production/main.o.d
@@ -1,5 +1,5 @@
 build/default/production/main.o:  \
-	C:/Users/rynmcd/Documents/GitHub/ee478/Final\ Project/RFIDexperimenting/RFIDbuff.X/main.c  \
-	C:/Users/rynmcd/Documents/GitHub/ee478/Final\ Project/RFIDexperimenting/RFIDbuff.X/rs232.h  \
-	C:/Users/rynmcd/Documents/GitHub/ee478/Final\ Project/RFIDexperimenting/RFIDbuff.X/interrupts.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Final\ Project/RFIDexperimenting/RFIDbuff.X/main.c  \
+	C:/Users/castia/Documents/GitHub/ee478/Final\ Project/RFIDexperimenting/RFIDbuff.X/rs232.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Final\ Project/RFIDexperimenting/RFIDbuff.X/interrupts.h  \
 
---
+++ Lab2/phase2Remote.X/build/default/debug/control.o.d.tmp
@@ -0,0 +1,6 @@
+build/default/debug/control.o:  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/control.c  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/SRAM.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/globals.h  \
+	C:/Users/castia/Documents/GitHub/ee478/Lab2/phase2Remote.X/interrupts.h  \
+
