b65ca6399f3131b2e075014cfd3a7e040123075e
castia <castillo.alyanna@gmail.com>
2014-05-27 15:28:16 -0700

    LED Driver
    Ready to test

--- Final Project/LED/ledDecoder.v
+++ Final Project/LED/ledDecoder.v
@@ -1,8 +1,10 @@
-module ledDecoder(outputBits, cardSelect, red, green);
-	output[7:0] outputBits /* synthesis LOC="P23P22P21P20P19P18P17" */;
-	input [1:0] cardSelect /* synthesis LOC="P2P3" */;
+module ledDecoder(outputBits, cardSelect, red, green, clk, rst);
+	output[7:0] outputBits /* synthesis LOC="P21P20P19P18P17P16P15P14" */;
+	input [1:0] cardSelect /* synthesis LOC="P3P2" */;
 	input red /* synthesis LOC="P4" */;
 	input green /* synthesis LOC="P5" */;
+	input clk /* synthesis LOC="P1" */;
+	input rst /* synthesis LOC="P13" */;
 
 	reg[7:0] outputBits;
 	
@@ -19,27 +21,33 @@ module ledDecoder(outputBits, cardSelect, red, green);
 	
 	*/
 	
-	always @ (red or green or cardSelect)
-	// Change card to be selected
-	case (cardSelect) 
-		0: begin
-				outputBits[0] = red;
-				outputBits[1] = green;
-			end
-		1: begin
-				outputBits[2] = red;
-				outputBits[3] = green;
-			end
-		2: begin
-				outputBits[4] = red;
-				outputBits[5] = green;
-			end
-		3: begin				
-				outputBits[6] = red;
-				outputBits[7] = green;
-			end
-		default: begin
-				outputBits[7:0] = 0;
-			end
-	endcase
+	always @ (posedge clk) begin
+		if (rst == 1) begin
+			outputBits[7:0] = { 0 };	
+		end
+		// Change card to be selected
+		else begin 
+			case (cardSelect) 
+				0: begin
+						outputBits[0] = red;
+						outputBits[1] = green;
+					end
+				1: begin
+						outputBits[2] = red;
+						outputBits[3] = green;
+					end
+				2: begin
+						outputBits[4] = red;
+						outputBits[5] = green;
+					end
+				3: begin				
+						outputBits[6] = red;
+						outputBits[7] = green;
+					end
+				default: begin
+						outputBits[7:0] = 0;
+					end
+			endcase
+		end
+	end
 endmodule
