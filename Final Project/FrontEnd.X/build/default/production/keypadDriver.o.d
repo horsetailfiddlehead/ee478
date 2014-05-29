df3ef0f3514cb95c6338d36ac06bdaf81dca7221
PatrickMa <patrick.is@not.here>
2014-05-28 18:48:21 -0700

    put correct project files in
    removed nbproject from ignore list

--- Final Project/FrontEnd.X/build/default/production/keypadDriver.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/keypadDriver.o:  \
-	D:/School/Senior/Spring/capstone/ee478/Final\ Project/FrontEnd.X/keypadDriver.c  \
-	D:/School/Senior/Spring/capstone/ee478/Final\ Project/FrontEnd.X/rs232.h  \
-	D:/School/Senior/Spring/capstone/ee478/Final\ Project/FrontEnd.X/keypadDriver.h  \
-
--- Final Project/FrontEnd.X/build/default/production/rs232.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/rs232.o:  \
-	D:/School/Senior/Spring/capstone/ee478/Final\ Project/FrontEnd.X/rs232.c  \
-	D:/School/Senior/Spring/capstone/ee478/Final\ Project/FrontEnd.X/rs232.h  \
-	D:/School/Senior/Spring/capstone/ee478/Final\ Project/FrontEnd.X/globals.h  \
-
